22c6f9132dbe2517022872b6ba85c746b4a3987b
Gabriel Monteiro <gmonteeeiro>
2018-08-19 14:40:16 -0300

    Adicionado FrmMain

---
+++ dynamicMenu/FrmMain.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace dynamicMenu
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pMenuLateral = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pbClose = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbClose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pMenuLateral
+            // 
+            this.pMenuLateral.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.pMenuLateral.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pMenuLateral.Location = new System.Drawing.Point(0, 0);
+            this.pMenuLateral.Name = "pMenuLateral";
+            this.pMenuLateral.Size = new System.Drawing.Size(227, 543);
+            this.pMenuLateral.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.panel1.Controls.Add(this.pbClose);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(227, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(687, 31);
+            this.panel1.TabIndex = 2;
+            // 
+            // pbClose
+            // 
+            this.pbClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.pbClose.Image = global::dynamicMenu.Properties.Resources.power_24x24;
+            this.pbClose.Location = new System.Drawing.Point(659, 3);
+            this.pbClose.Name = "pbClose";
+            this.pbClose.Size = new System.Drawing.Size(25, 25);
+            this.pbClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbClose.TabIndex = 0;
+            this.pbClose.TabStop = false;
+            this.pbClose.Click += new System.EventHandler(this.pbClose_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 543);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pMenuLateral);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmMain";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbClose)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pMenuLateral;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pbClose;
+    }
+}
+
---
+++ dynamicMenu/FrmMain.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dynamicMenu
+{
+    public partial class FrmMain : Form
+    {
+
+        private int hBtn = 40;
+        private int wBtn = 209;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            CarregaMenu();
+        }
+
+        private void CarregaMenu()
+        {
+            //Tamanho da tela
+            int yTela = this.Size.Height;
+
+            //Quantidade de itens no menu
+            int qtd = 4;
+
+            String nome = "teste";
+
+            int pos1 = ((yTela - qtd * hBtn) / 2) - (hBtn/2);
+
+            for (int i = 0; i < qtd; i++)
+            {
+                int pos = pos1 + (i * hBtn);
+
+                String icnNome = $"icn{nome}{i}";
+                String btnNome = $"{nome}{i}";
+
+                AddIcn(pos, icnNome);
+                AddButton(pos, btnNome, "Texto" + i);
+            }
+        }
+
+        /// <summary>
+        /// Adiciona ícone lateral
+        /// </summary>
+        /// <param name="y"></param>
+        /// <param name="name"></param>
+        private void AddIcn(int y, String name)
+        {
+            Panel p = new Panel
+            {
+                Anchor = (AnchorStyles.Left | AnchorStyles.Top),
+                BackColor = Color.Green,
+                Name = name,
+                TabStop = false,
+                Size = new Size(10, hBtn),
+                Location = new Point(0, y),
+                Visible = false
+            };
+            pMenuLateral.Controls.Add(p);
+        }
+
+        /// <summary>
+        /// Adiciona Botão no menu
+        /// </summary>
+        /// <param name="y"></param>
+        /// <param name="name"></param>
+        /// <param name="desc"></param>
+        private void AddButton(int y, String name, String desc)
+        {
+            Button b = new Button
+            {
+                Anchor = (AnchorStyles.Left | AnchorStyles.Top),
+                FlatStyle = FlatStyle.Flat,
+                Location = new Point(10, y),
+                Font = new Font("Microsoft Sans Serif", 10F),
+                ForeColor = Color.FromArgb(217, 217, 217),
+                Name = name,
+                Size = new Size(wBtn, hBtn),
+                TabStop = true,
+                Text = desc
+            };
+            b.FlatAppearance.BorderSize = 0;
+
+            b.Click += new EventHandler(MenuClick);
+            pMenuLateral.Controls.Add(b);
+        }
+
+        /// <summary>
+        /// Click de alguma opção do menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuClick(object sender, EventArgs e)
+        {
+            //Deixa todos com a cor padrão
+            foreach (Control p in pMenuLateral.Controls)
+            {
+                if (p is Panel)
+                {
+                    p.Visible = false;
+                }
+            }
+
+            Button b = (Button)sender;
+            String pName = "icn" + b.Name;
+
+            //Muda a cor do ícone
+            Panel icn = pMenuLateral.Controls.Find(pName, false).FirstOrDefault() as Panel;
+            icn.Visible = true;
+        }
+
+        private void pbClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
