bb40662b4f6b1264287d0b0ac869eb45d3882167
Gabriel Monteiro <gmonteeeiro@gmail.com>
2018-08-19 19:54:37 -0300

    Salvar XML de configuração

--- dynamicMenu/Config.Designer.cs
+++ dynamicMenu/Config.Designer.cs
@@ -28,19 +28,217 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.bSave = new System.Windows.Forms.Button();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.line1 = new System.Windows.Forms.Panel();
+            this.bClose = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bClose)).BeginInit();
             this.SuspendLayout();
+            // 
+            // bSave
+            // 
+            this.bSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(53)))), ((int)(((byte)(56)))));
+            this.bSave.FlatAppearance.BorderSize = 0;
+            this.bSave.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.bSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.bSave.ForeColor = System.Drawing.Color.White;
+            this.bSave.Location = new System.Drawing.Point(49, 181);
+            this.bSave.Name = "bSave";
+            this.bSave.Size = new System.Drawing.Size(294, 40);
+            this.bSave.TabIndex = 5;
+            this.bSave.Text = "Salvar";
+            this.bSave.UseVisualStyleBackColor = false;
+            this.bSave.Click += new System.EventHandler(this.bSave_Click);
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbAddress.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.tbAddress.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tbAddress.ForeColor = System.Drawing.Color.White;
+            this.tbAddress.Location = new System.Drawing.Point(97, 48);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(270, 23);
+            this.tbAddress.TabIndex = 1;
+            this.tbAddress.Enter += new System.EventHandler(this.TextBox_Enter);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(372, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Configurações do Banco de Dados";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // line1
+            // 
+            this.line1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.line1.Location = new System.Drawing.Point(0, 20);
+            this.line1.Name = "line1";
+            this.line1.Size = new System.Drawing.Size(392, 1);
+            this.line1.TabIndex = 5;
+            // 
+            // bClose
+            // 
+            this.bClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.bClose.Image = global::dynamicMenu.Properties.Resources.power_24x24;
+            this.bClose.Location = new System.Drawing.Point(372, 0);
+            this.bClose.Name = "bClose";
+            this.bClose.Size = new System.Drawing.Size(20, 20);
+            this.bClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bClose.TabIndex = 6;
+            this.bClose.TabStop = false;
+            this.bClose.Click += new System.EventHandler(this.bClose_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(24, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Endereço";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // tbUser
+            // 
+            this.tbUser.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.tbUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tbUser.ForeColor = System.Drawing.Color.White;
+            this.tbUser.Location = new System.Drawing.Point(97, 77);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(270, 23);
+            this.tbUser.TabIndex = 2;
+            this.tbUser.Enter += new System.EventHandler(this.TextBox_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(36, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Usuário";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // tbPass
+            // 
+            this.tbPass.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbPass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.tbPass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tbPass.ForeColor = System.Drawing.Color.White;
+            this.tbPass.Location = new System.Drawing.Point(97, 106);
+            this.tbPass.Name = "tbPass";
+            this.tbPass.Size = new System.Drawing.Size(270, 23);
+            this.tbPass.TabIndex = 3;
+            this.tbPass.Enter += new System.EventHandler(this.TextBox_Enter);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(44, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Senha";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // tbPort
+            // 
+            this.tbPort.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbPort.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.tbPort.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tbPort.ForeColor = System.Drawing.Color.White;
+            this.tbPort.Location = new System.Drawing.Point(97, 135);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(270, 23);
+            this.tbPort.TabIndex = 4;
+            this.tbPort.Text = "3306";
+            this.tbPort.Enter += new System.EventHandler(this.TextBox_Enter);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(51, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Porta";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // Config
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(304, 234);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.ClientSize = new System.Drawing.Size(392, 240);
+            this.Controls.Add(this.bClose);
+            this.Controls.Add(this.line1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.tbPass);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.tbAddress);
+            this.Controls.Add(this.bSave);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Config";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Config";
+            ((System.ComponentModel.ISupportInitialize)(this.bClose)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button bSave;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel line1;
+        private System.Windows.Forms.PictureBox bClose;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbPass;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Label label5;
     }
 }
--- dynamicMenu/Config.cs
+++ dynamicMenu/Config.cs
@@ -7,11 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace dynamicMenu
 {
     public partial class Config : Form
     {
+        private String configFile = "config.xml";
+
+        public String dbAddress { get; private set; }
+        public String dbUser { get; private set; }
+        public String dbPass { get; private set; }
+        public String dbPort { get; private set; }
+
         /// <summary>
         /// Construtor
         /// </summary>
@@ -19,7 +28,7 @@ namespace dynamicMenu
         {
             InitializeComponent();
 
-            if (System.IO.File.Exists("config.xml"))
+            if (System.IO.File.Exists(configFile))
             {
                 LoadConfig();
             }
@@ -32,12 +41,65 @@ namespace dynamicMenu
 
         private void LoadConfig()
         {
-
+          
         }
 
         private void CreateConfig()
         {
             ShowDialog();
+
+            #region Salva XML
+
+            XmlDocument doc = new XmlDocument();
+
+            XmlElement xeConfig = doc.CreateElement("config");
+
+            XmlElement xeAddress = doc.CreateElement("address");
+            xeAddress.InnerText = dbAddress;
+            xeConfig.AppendChild(xeAddress);
+
+            XmlElement xeUser = doc.CreateElement("user");
+            xeUser.InnerText = dbUser;
+            xeConfig.AppendChild(xeUser);
+
+            XmlElement xePass = doc.CreateElement("password");
+            xePass.InnerText = dbPass;
+            xeConfig.AppendChild(xePass);
+
+            XmlElement xePort = doc.CreateElement("port");
+            xePort.InnerText = dbPort;
+            xeConfig.AppendChild(xePort);
+
+            doc.AppendChild(xeConfig);
+            doc.Save(configFile);
+
+            #endregion
+
+        }
+
+        #region Métodos dos Botões
+
+        private void bClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void bSave_Click(object sender, EventArgs e)
+        {
+            dbAddress = tbAddress.Text;
+            dbPass = tbPass.Text;
+            dbPort = tbPort.Text;
+            dbUser = tbUser.Text;
+
+            this.Close();
+        }
+
+        #endregion
+
+        private void TextBox_Enter(object sender, EventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            tb.SelectAll();
         }
     }
 }
